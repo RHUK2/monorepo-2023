58f1c43d74c9410f1f2fb3ad3755be639e4e64c2
huryu <huryu@wecruitcorp.com>
2023-06-26 17:09:08 +0900

    #INIT tsconfig 수정

--- apps/service/app/page.tsx
+++ apps/service/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@shared/components';
 import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Button />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
